e8f27470530f6be5d6c5a70633ef7a374b548e09
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 09:27:57 -0300

    fix: create, delete and update

--- src/main/java/br/com/brunobrolesi/parking/service/ParkingService.java
+++ src/main/java/br/com/brunobrolesi/parking/service/ParkingService.java
@@ -19,9 +19,11 @@ public class ParkingService {
     @Autowired
     private ParkingRepository parkingRepository;
     @Autowired
-    private AddressRepository addressRepository;
+    private AddressService addressService;
     @Autowired
     private ParkingSpaceRepository parkingSpaceRepository;
+    @Autowired
+    private ParkingSpaceService parkingSpaceService;
 
     public List<Parking> findAll() {
         List<Parking> parkingList = parkingRepository.findAll();
@@ -32,39 +34,43 @@ public class ParkingService {
     }
 
     public Parking findById(Integer id) {
-        Optional<Parking> optional = parkingRepository.findById(id);
+        Optional<Parking> entity = parkingRepository.findById(id);
 
-        if (optional.isEmpty()) throw new EmptyResultDataAccessException("Nenhum resultado encontrado", 1);
+        if (entity.isEmpty()) throw new EmptyResultDataAccessException("Nenhum resultado encontrado", 1);
 
-        return optional.get();
+        return entity.get();
     }
 
     public Parking create(Parking parking) {
-        Address address = parking.getAddress();
-        List<ParkingSpace> parkingSpaces = parking.getParkingSpaces();
 
-        Parking inserted = parkingRepository.save(parking);
-        addressRepository.save(address);
-        parkingSpaceRepository.saveAll(parkingSpaces);
-
-        return inserted;
+        addressService.create(parking.getAddress());
+        Parking created = parkingRepository.save(parking);
+        parkingSpaceService.createMany(parking.getParkingSpaces());
+        return created;
     }
 
     public void delete(Integer id) {
+        Optional<Parking> entity = parkingRepository.findById(id);
+
+        if (entity.isEmpty()) throw new IllegalArgumentException("Este id não é valido");
+
         parkingRepository.deleteById(id);
     }
 
     public Parking update(Integer id, Parking obj) {
         Optional<Parking> entity = parkingRepository.findById(id);
-        if (entity.isPresent()) {
-            updateData(entity.get(), obj);
-            return parkingRepository.save(entity.get());
-        }
-        return null;
+
+        if (entity.isEmpty()) throw new IllegalArgumentException("Este id não é valido");
+
+        updateData(entity.get(), obj);
+        return parkingRepository.save(entity.get());
     }
 
     private void updateData(Parking entity, Parking obj) {
         entity.setName(obj.getName());
+        entity.getAddress().setStreet(obj.getAddress().getStreet());
+        entity.getAddress().setNumber(obj.getAddress().getNumber());
+        entity.getAddress().setAddress_2(obj.getAddress().getAddress_2());
         entity.setPhones(obj.getPhones());
     }
 }
