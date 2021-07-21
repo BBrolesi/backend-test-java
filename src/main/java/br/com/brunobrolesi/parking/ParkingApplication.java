93479aca02fd69bcb851dede9baa4cf18dbc2662
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-21 08:52:43 -0300

    fix: add only states and cites

--- src/main/java/br/com/brunobrolesi/parking/ParkingApplication.java
+++ src/main/java/br/com/brunobrolesi/parking/ParkingApplication.java
@@ -36,12 +36,6 @@ public class ParkingApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Vehicle v1 = new Vehicle(null,"FORD","Fusion","2018","Preto","ALX1282",VehicleType.CARRO);
-		Vehicle v2 = new Vehicle(null,"Ford","Ka","2014","Branco","ALX8182",VehicleType.CARRO);
-		Vehicle v3 = new Vehicle(null,"volkswagen","gol","2020","Azul","ALX4182",VehicleType.CARRO);
-
-		vehicleRepository.saveAll(Arrays.asList(v1, v2, v3));
-
 		State s1 = new State(null, "São Paulo");
 		State s2 = new State(null, "Paraná");
 
@@ -54,24 +48,5 @@ public class ParkingApplication implements CommandLineRunner {
 
 		stateRepository.saveAll(Arrays.asList(s1, s2));
 		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
-
-		Parking p1 = new Parking(null,"555.555.555", "Estacionamento Central");
-		p1.getPhones().addAll(Arrays.asList("2231-4323", "33432-34234"));
-
-		Address a1 = new Address(null,"Avenida Brasil","333",null, p1, c1);
-		Address a2 = new Address(null,"Avenida Matos","3233",null, p1, c3);
-
-		p1.getAddresses().addAll(Arrays.asList(a1, a2));
-
-		parkingRepository.saveAll(Arrays.asList(p1));
-		addressRepository.saveAll(Arrays.asList(a1, a2));
-
-		ParkingSpace ps1 = new ParkingSpace(null, VehicleType.CARRO, p1);
-		ParkingSpace ps2 = new ParkingSpace(null, VehicleType.CARRO, p1);
-		ParkingSpace ps3 = new ParkingSpace(null, VehicleType.MOTO, p1);
-
-		p1.getParkingSpaces().addAll(Arrays.asList(ps1, ps2, ps3));
-
-		parkingSpaceRepository.saveAll(Arrays.asList(ps1, ps2, ps3));
 	}
 }
